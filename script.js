544a4e1efd648fb8de8529d71178dcbcd4bcfaf0
Rockey Kumar <104070946+rockeykumarmuz@users.noreply.github.com>
2023-09-09 01:43:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function fibonacci(num) {
+function fibonacci(n) {
 // your code here
+	let n1 = 0;
+	let n2 = 1;
+	if(n==1) return 0;
+	else if(n==2) return 1;
+
+	let sum = 0;
+
+	for(let i=2; i<=n; i++) {
+		sum = n1 + n2;
+		n1 = n2;
+		n2 = sum;
+	}
+
+	return sum;
 }
 
 module.exports = fibonacci;
